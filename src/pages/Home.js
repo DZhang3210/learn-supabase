32f554946fce1c696d280595aec998a18d0bd9a0
DZhang3210 <davidzhang3210@gmail.com>
2024-09-03 11:42:42 -0400

    new commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,41 @@
+import { useEffect, useState } from "react";
+import supabase from "../config/supabaseClient";
+
 const Home = () => {
+  const [fetchError, setFetchError] = useState(null);
+  const [smoothies, setSmoothies] = useState(null);
+  useEffect(() => {
+    const fetchSmoothies = async () => {
+      const { data, error } = await supabase.from("smoothies").select();
+
+      if (error) {
+        setFetchError("Could not fetch smoothies");
+        setSmoothies(null);
+        console.log(error);
+      }
+
+      if (data) {
+        setSmoothies(data);
+        setFetchError(null);
+        console.log(data);
+      }
+    };
+
+    fetchSmoothies();
+  }, []);
   return (
     <div className="page home">
-      <h2>Home</h2>
-    </div>
-  )
-}
+      {fetchError && <p className="text-black">{fetchError}</p>}
 
-export default Home
+      {smoothies && (
+        <div className="smoothies text-black">
+          {smoothies.map((smoothie) => (
+            <p>{smoothie.title}</p>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Home;
